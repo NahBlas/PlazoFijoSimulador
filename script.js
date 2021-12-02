ff61c14acd4620975e61d762ea37bb4fcdd73715
Nahuel Blasquiz <neblasquiz@gmail.com>
2021-12-02 02:39:22 -0300

    Primera subida de codigo

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('linkeado')
+//Solicito Nombre de usuario
+let nombreUsuario = prompt('Ingrese su nombre')
+// Mensajes al usuario
+alert ('Bienvenido  ' + nombreUsuario )
+alert ('1--Deseo Calcular plazo fijo  //   2--No deseo calcular plazo fijo')
+//Consigo una opcion para ingresar al programa
+let opcion = prompt('Ingrese la opcion')
+//validacion para entrada al ciclo de ejecucion de calculo
+if (opcion == '1'){
+accesso = true
+console.log(accesso)
+}
+
+while (accesso == true){
+let monto = prompt('Ingrese el monto para su plazo fijo')
+
+}
+
+function calcularPlazo (){
+    
+}
+
+
