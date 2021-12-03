b4cd957e9cc06c9d488e991ba8e362b9987bca9e
Nahuel Blasquiz <neblasquiz@gmail.com>
2021-12-03 16:42:29 -0300

    Debbug variables no declaradas

--- script.js
+++ script.js
@@ -13,39 +13,36 @@ alert ('1--Deseo Calcular plazo fijo  //   2--No deseo calcular plazo fijo')
 //Consigo una opcion para ingresar al programa
 let opcion = prompt('Ingrese la opcion')
 
+let acceso = Boolean; //defino variable para su uso
 
 //validacion para entrada al ciclo de ejecucion de calculo
 if (opcion == '1'){
-accesso = true
-console.log(accesso)
+acceso = true
+console.log(acceso)
 }
 
+//declaro variables
+let tip = 0
+let montoFinal = 0
 
 //procedure principal.
-while (accesso == true){
+while (acceso == true){
 let monto = prompt('Ingrese el monto para su plazo fijo')
 console.log(monto)
 let plazo = prompt('ingrese plazo en dias 30-90-180-365')
 console.log(plazo)
-calcularTna(tip, plazo)
-calcularTotal(montoFinal, monto, tip, plazo)
+tip = calcularTna(plazo)
+montoFinal = calcularTotal(monto, tip, plazo)
 alert ('Su plata final es de ' + montoFinal)
 }
 
-
-// defino variables para almacenar datos de funciones en ellas.
-let montoFinal = 0
-//creo funcion para calcular pasando los parametros segun los distintos tipos de Montos,TIP (Taza interes por plazo) y plazo
-function calcularTotal (montoFinal, monto, tip, plazo){
-    montoFinal = monto*(tip*plazo/365)
-}
-
-
-// defino variables para almacenar datos de funciones en ellas.
-let tip = 0
 // creo funcion de calculo de la tna
-function calcularTna(tip, plazo){
-    tip = plazo*30/12
+function calcularTna(plazo){
+    return plazo * 30 / 12;
 }
 
+//creo funcion para calcular pasando los parametros segun los distintos tipos de Montos,TIP (Taza interes por plazo) y plazo
+function calcularTotal (monto, tip, plazo){
+    return monto * (tip * plazo / 365)
+}
 
